31f93ad3e8e82b1c42734be3545cc55d230f2dc9
NavisGames <lakendianer@gmail.com>
2022-12-16 19:09:29 +0100

    Adding new function for later Adding new imports for later

--- ValorantTracking/functions.py
+++ ValorantTracking/functions.py
@@ -1,9 +1,17 @@
+from typing import List
+
 import httpx as http
 from itertools import accumulate
 from PyQt5.QtCore import Qt
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPalette, QColor, QImage, QPixmap, QFontDatabase
 from PyQt5.QtWidgets import QApplication
+
+import valo_api
+from valo_api.endpoints.raw import EndpointType
+from valo_api.responses.match_history import MatchHistoryPointV3
+
+current_season = "E5A3"
 
 intervals = (
     ("Weeks", 604800),  # 60 * 60 * 24 * 7
@@ -58,3 +66,28 @@ def findStatsOfPlayer(player, players):
 def findAgentOfPlayer(player, players):
     agent = list(accumulate(p.character for p in players.all_players if p.name == player))
     return agent if agent else None
+
+
+def get_matches(region: str, puuid: str) -> List[MatchHistoryPointV3]:
+    step_size = 20  # maximum is 20
+
+    match_ids = set()
+    query_args = {
+        "startIndex": 0,
+        "endIndex": step_size,
+    }
+    while True:
+        match_history = valo_api.get_raw_data_v1(
+            EndpointType.MATCH_HISTORY, region=region, value=puuid, queries=query_args
+        )
+        for match in match_history.History:
+            match_ids.add(match.MatchID)
+
+        if query_args["endIndex"] >= match_history.Total:
+            # All matches processed
+            break
+        # Update query args. Do the next pagination step.
+        query_args["startIndex"] = query_args["endIndex"] + 1
+        query_args["endIndex"] = min(query_args["endIndex"] + step_size, match_history.Total)
+
+    return [valo_api.get_match_details_v2(match_id=match_id) for match_id in match_ids]
