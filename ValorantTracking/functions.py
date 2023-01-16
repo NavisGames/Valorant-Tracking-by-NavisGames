5e271e6158742fb513e011cbaea6b272134317f7
NavisGames <lakendianer@gmail.com>
2023-01-16 14:36:57 +0100

    Removed Function

--- ValorantTracking/functions.py
+++ ValorantTracking/functions.py
@@ -103,16 +103,6 @@ def findAgentOfPlayer(player: str, players: list):
     return {p.name: p.character for p in players}.get(player)
 
 
-def highestRank(puuID: str, region: str):
-    mmr_details = valo_api.get_mmr_details_by_puuid(
-        version="v2", region=region, puuid=puuID
-    )
-    return max(
-        (season.act_rank_wins for season in mmr_details.by_season.values()),
-        key=lambda x: x[0].tier if x else -1,
-    )[0].patched_tier
-
-
 def get_matches(region: str, puuid: str) -> List[MatchHistoryPointV3]:
     step_size = 20  # maximum is 20
 
